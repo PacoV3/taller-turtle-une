e4c9bf2e5c57be02d093bc75f394d3499da10772
Carlos-M301 <60450030+Carlos-M301@users.noreply.github.com>
2020-04-27 18:52:35 -0500

    Create For.py

---
+++ Ejemplos de Ciclos/For.py
@@ -0,0 +1,13 @@
+import turtle 
+
+polygon = turtle.Turtle()
+
+num_sides = int(input("Numero de lados: "))
+side_length = int(input("Longitud: "))
+angle = 360.0 / num_sides 
+
+for i in range(num_sides):
+    polygon.forward(side_length)
+    polygon.right(angle)
+
+turtle.done()
